371c991f328eabab628eab65571e7c409e95dbe2
it025 <dkusumanjali20050@gmail.com>
2026-04-05 16:18:37 +0530

    Add files via upload

---
+++ TestConnection.java
@@ -0,0 +1,17 @@
+package db;
+import java.sql.Connection;
+
+public class TestConnection {
+   public TestConnection() {
+   }
+
+   public static void main(String[] var0) {
+      Connection var1 = DBConnection.getConnection();
+      if (var1 != null) {
+         System.out.println("Database Connected Successfully");
+      } else {
+         System.out.println("Connection Failed");
+      }
+
+   }
+}
